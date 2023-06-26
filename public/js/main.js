531f8c0b6ccdf0822e661576ee32b60533c76530
David900Jason <87255629+David900Jason@users.noreply.github.com>
2023-06-26 11:16:41 +0300

    Finished the last touch

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
+const questionBtns = document.querySelectorAll(".question");
 
+questionBtns.forEach(button => {
+  button.addEventListener("click", () => {
+    if (button.nextElementSibling.dataset.close == "false") {
+      button.nextElementSibling.dataset.close = "true";
+      button.classList.remove("open-menu");
+    } else if (button.nextElementSibling.dataset.close == "true") {
+      button.nextElementSibling.dataset.close = "false";
+      button.classList.add("open-menu");
+    }
+  });
+});
